bf3a0cfa3f593b0d295e7114fa4470cf8456c7dd
Vishnu <vishnureddy0010@gmail.com>
2024-12-24 18:43:53 +0530

    Added Delete Functionality

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-complete",
-  password: "Newtyork@1234",
+  password: "Newyork@1234",
 });
 
 module.exports = pool.promise();
